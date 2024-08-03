d9082cc74c451dc1f69141f25650a5a533da98ad
ThePrincipleMan <96772826+ThePrincipleMan@users.noreply.github.com>
2024-08-03 11:35:48 +0530

    Change backend link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {io} from "socket.io-client"
 
 function Home(){
-    const socket = io("http://localhost:3001")
+    const socket = io.connect("https://chamful.onrender.com", {'timeout':500000000, 'connect timeout': 500000000})
 
   return (
     <>
